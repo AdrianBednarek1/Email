d127b7d32a1a19ab6d24ccd5cd087c342ecadcc4
AdrianBednarek1 <add.bednarek@gmail.com>
2023-01-30 22:01:58 +0100

    uredzivnaje

--- Email/Entity/Person.cs
+++ Email/Entity/Person.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName{ get; set; }
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
         public string BirthDate { get; set; }
         public int UserAccountId { get; set; }
         public User UserAccount { get; set; }
